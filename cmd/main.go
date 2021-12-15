fc3e4943e1cbea6c3c901f42aa6e8e72cb686982
Matheus <mrehbein45@gmail.com>
2021-12-15 16:55:04 -0300

    create service

---
+++ cmd/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/retatu/fullcycle-gateway/adapter/broker/kafka"
+	"github.com/retatu/fullcycle-gateway/adapter/factory"
+	"github.com/retatu/fullcycle-gateway/adapter/presenter/transaction"
+	"github.com/retatu/fullcycle-gateway/usecase/process_transaction"
+)
+
+func main() {
+	//db
+	db, err := sql.Open("sqlite3", "test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//repository
+	repositoryFactory := factory.NewRepositoryDatabaseFactory(db)
+	repository := repositoryFactory.CreateTransactionRepository()
+	//confiMapProducer
+	configMapProducer := &ckafka.ConfigMap{
+		"bootstrap.servers": "kafka:9092",
+	}
+	kafkaPresenter := transaction.NewTransactionKafkaPresenter()
+	//producer
+	producer := kafka.NewKafkaProducer(configMapProducer, kafkaPresenter)
+	//confiMapConsumer
+	configMapConsumer := &ckafka.ConfigMap{
+		"bootstrap.servers": "kafka:9092",
+		"client.id":         "goapp",
+		"group.id":          "goapp",
+	}
+	//topic
+	topics := []string{"transactions"}
+	//consumer
+	var msgChan = make(chan *ckafka.Message)
+	consumer := kafka.NewConsumer(configMapConsumer, topics)
+	go consumer.Consume(msgChan)
+
+	//usecase
+	usecase := process_transaction.NewProcessTransaction(repository, producer, "transactions_result")
+	for msg := range msgChan {
+		var input process_transaction.TransactionDtoInput
+		json.Unmarshal(msg.Value, &input)
+		usecase.Execute(input)
+	}
+}
--- usecase/process_transaction/transaction_dto.go
+++ usecase/process_transaction/transaction_dto.go
@@ -1,14 +1,14 @@
 package process_transaction
 
 type TransactionDtoInput struct {
-	ID                        string  `json: "id"`
-	AccountID                 string  `json: "account_id"`
-	CreditCardNumber          string  `json: "credit_card_number"`
-	CreditCardName            string  `json: "credit_card_name"`
-	CreditCardExpirationMonth int     `json: "credit_card_expiration_month"`
-	CreditCardExpirationYear  int     `json: "credit_card_expiration_year"`
-	CreditCardCvv             int     `json: "credit_card_cvv"`
-	Amount                    float64 `json: "amount"`
+	ID                        string  `json:"id"`
+	AccountID                 string  `json:"account_id"`
+	CreditCardNumber          string  `json:"credit_card_number"`
+	CreditCardName            string  `json:"credit_card_name"`
+	CreditCardExpirationMonth int     `json:"credit_card_expiration_month"`
+	CreditCardExpirationYear  int     `json:"credit_card_expiration_year"`
+	CreditCardCvv             int     `json:"credit_card_cvv"`
+	Amount                    float64 `json:"amount"`
 }
 
 type TransactionDtoOutput struct {
